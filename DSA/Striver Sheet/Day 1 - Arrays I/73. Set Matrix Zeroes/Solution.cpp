bb247c0b97094e5d5d67c7666bbdaf9aa23170e0
maneshantanu20 <manesg@rknec.edu>
2023-06-01 14:16:45 +0530

    ques: arrays - I - set zeroes py done : master

---
+++ DSA/Striver Sheet/Day 1 - Arrays I/73. Set Matrix Zeroes/Solution.cpp
@@ -0,0 +1,25 @@
+//
+// Created by SHANTANU on 31-05-2023.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector <vector<int>> &matrix) {
+        
+    }
+};
+
+int main() {
+
+    vector <vector<int>> matrix = {{1, 0, 2},
+                                   {4, 1, 3}};
+
+    Solution solution;
+    solution.setZeroes(matrix);
+
+
+    return 0;
+}
