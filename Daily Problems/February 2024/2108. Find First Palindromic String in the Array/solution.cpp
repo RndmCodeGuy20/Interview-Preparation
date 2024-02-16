bcd8d06cfecbb395791120d38bc61233ce2678f3
Shantanu Mane <shantanu.mane.200@outlook.com>
2024-02-16 22:32:41 +0530

    chore: daily solutions : master

--- Daily Problems/February 2024/2108. Find First Palindromic String in the Array/solution.cpp
+++ Daily Problems/February 2024/2108. Find First Palindromic String in the Array/solution.cpp
@@ -1,19 +1,28 @@
-
 //
 // Created by SHANTANU on 2024-02-13.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
-    string firstPalindrome(vector<string>& words) {
-        
+    string firstPalindrome(vector<string> &words) {
+        for (auto &word: words) {
+            string reversed = word;
+            reverse(reversed.begin(), reversed.end());
+            if (reversed == word) {
+                return word;
+            }
+        }
+        return "";
     }
 };
 
 int main() {
     Solution solution;
+    vector<string> words = {"abc", "def", "aba"};
+    cout << solution.firstPalindrome(words) << endl;
 
     return 0;
 }
--- Daily Problems/February 2024/2149. Rearrange Array Elements by Sign/solution.cpp
+++ Daily Problems/February 2024/2149. Rearrange Array Elements by Sign/solution.cpp
@@ -3,17 +3,50 @@
 // Created by SHANTANU on 2024-02-14.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        
+    vector<int> rearrangeArray(vector<int> &nums) {
+//        vector<int> posArr;
+//        vector<int> negArr;
+//
+//        for (int num: nums) {
+//            if (num > 0) {
+//                posArr.push_back(num);
+//            } else {
+//                negArr.push_back(num);
+//            }
+//        }
+
+        vector<int> res(nums.size(), 0);
+        int pos = 0;
+        int neg = 1;
+
+        for (int num: nums) {
+            if (num > 0) {
+                res[pos] = num;
+                pos += 2;
+            } else {
+                res[neg] = num;
+                neg += 2;
+            }
+        }
+
+        return res;
     }
 };
 
 int main() {
     Solution solution;
+    vector<int> in = {3, 1, -2, -5, 2, -4};
+
+    vector<int> res = solution.rearrangeArray(in);
+
+    for (int num: res) {
+        cout << num << " ";
+    }
 
     return 0;
 }
