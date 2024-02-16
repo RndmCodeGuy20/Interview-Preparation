56306542ec3e988f976f33219a8f0f7d154a7af7
Shantanu Mane <shantanu.mane.200@outlook.com>
2024-02-16 23:05:08 +0530

    chore: daily 16/2 : master

--- Daily Problems/February 2024/1481. Least Number of Unique Integers after K Removals/solution.cpp
+++ Daily Problems/February 2024/1481. Least Number of Unique Integers after K Removals/solution.cpp
@@ -3,17 +3,47 @@
 // Created by SHANTANU on 2024-02-16.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
-    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        
+    int findLeastNumOfUniqueInts(vector<int> &arr, int k) {
+        map<int, int> numCounts;
+
+        for (int num: arr) {
+            numCounts[num]++;
+        }
+
+        vector<int> freq;
+        for (pair<int, int> counts: numCounts) {
+            freq.push_back(counts.second);
+        }
+
+        sort(freq.begin(), freq.end());
+
+        for (int i = 0; i < freq.size(); ++i) {
+            if (k > freq[i]) {
+                k -= freq[i];
+                freq[i] = 0;
+            } else {
+                freq[i] -= k;
+                k = 0;
+            }
+        }
+        int count = 0;
+        for (int i: freq) {
+            if (i != 0)count++;
+        }
+
+        return count;
     }
 };
 
 int main() {
     Solution solution;
+    vector<int> arr = {4, 3, 1, 1, 3, 3, 2};
+    cout << solution.findLeastNumOfUniqueInts(arr, 3);
 
     return 0;
 }
