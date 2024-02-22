1dbadb80b3e5d750328ae18b4a672aaf487b0e2a
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2024-02-22 01:37:38 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2024/997. Find the Town Judge/solution.cpp
@@ -0,0 +1,19 @@
+
+//
+// Created by SHANTANU on 2024-02-22.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+
+    return 0;
+}
