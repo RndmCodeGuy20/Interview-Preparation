e73c79d5a8eb4630a1f548c24c7f3eb65e1f1eef
Shantanu Mane <shantanu.mane.200@outlook.com>
2024-02-22 13:46:03 +0530

    chore: daily 20/2 : master

--- Daily Problems/February 2024/1642. Furthest Building You Can Reach/solution.cpp
+++ Daily Problems/February 2024/1642. Furthest Building You Can Reach/solution.cpp
@@ -3,17 +3,34 @@
 // Created by SHANTANU on 2024-02-17.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        
+    int furthestBuilding(vector<int> &heights, int bricks, int ladders) {
+        int rez = 0;
+
+        for (int i = 0; i < heights.size() - 1; ++i) {
+            if (heights[i] < heights[i + 1]) {
+                if (abs(heights[i] - heights[i + 1]) > (bricks / 2) && ladders > 0) {
+                    ladders--;
+                    rez++;
+                } else {
+                    bricks -= abs(heights[i] - heights[i + 1]);
+                    rez++;
+                }
+            }
+        }
+
+        return rez;
     }
 };
 
 int main() {
     Solution solution;
+    vector<int> heights = {4, 12, 2, 7, 3, 18, 20, 3, 19};
+    cout << solution.furthestBuilding(heights, 10, 2);
 
     return 0;
 }
--- Daily Problems/February 2024/201. Bitwise AND of Numbers Range/solution.cpp
+++ Daily Problems/February 2024/201. Bitwise AND of Numbers Range/solution.cpp
@@ -3,17 +3,26 @@
 // Created by SHANTANU on 2024-02-21.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        
+        int count = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            count++;
+        }
+
+        return left <<= count;
     }
 };
 
 int main() {
     Solution solution;
+    cout << solution.rangeBitwiseAnd(5, 7);
 
     return 0;
 }
