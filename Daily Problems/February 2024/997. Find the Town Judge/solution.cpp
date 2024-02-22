30e5cab70c6e78d2fa742c856bc4aa2cbaadf4f5
Shantanu Mane <shantanu.mane.200@outlook.com>
2024-02-22 14:12:19 +0530

    chore: daily 22/2 : master

--- Daily Problems/February 2024/997. Find the Town Judge/solution.cpp
+++ Daily Problems/February 2024/997. Find the Town Judge/solution.cpp
@@ -3,17 +3,35 @@
 // Created by SHANTANU on 2024-02-22.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        
+    int findJudge(int n, vector<vector<int>> &trust) {
+        map<int, int> incoming;
+        map<int, int> outgoing;
+
+        for (vector<int> t: trust) {
+            outgoing[t[0]]++;
+            incoming[t[1]]++;
+        }
+
+        for (int i = 1; i < n + 1; ++i) {
+            if (incoming[i] == (n - 1) && outgoing[i] == 0) return i;
+        }
+
+        return -1;
     }
 };
 
 int main() {
     Solution solution;
+    vector<vector<int>> trust = {{1, 3},
+                                 {2, 3},
+                                 {3, 1}};
+
+    cout << solution.findJudge(3, trust);
 
     return 0;
 }
