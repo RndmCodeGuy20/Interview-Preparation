dec4294f20d77d00f98632855e55fa3956014d9f
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-01-30 13:19:19 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/January 2025/2493. Divide Nodes Into the Maximum Number of Groups/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
