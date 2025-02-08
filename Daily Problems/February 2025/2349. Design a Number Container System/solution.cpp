2eca9c01dc7b97e979291be91b1a015ec2e7bd2f
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-02-08 02:11:17 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2025/2349. Design a Number Container System/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class NumberContainers {
+public:
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        
+    }
+    
+    int find(int number) {
+        
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
+
+int main() {
+    Solution solution;
+    return 0;
+}
