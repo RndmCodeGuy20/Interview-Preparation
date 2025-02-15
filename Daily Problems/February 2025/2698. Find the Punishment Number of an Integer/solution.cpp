4843191c1efdab2d57e3a1e59e8e35f13dc19a99
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-02-15 02:14:11 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2025/2698. Find the Punishment Number of an Integer/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int punishmentNumber(int n) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
