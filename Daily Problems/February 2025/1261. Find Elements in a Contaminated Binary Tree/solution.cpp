907133cd1a7f6ed210acb5855ac6f6def4a11413
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-02-21 02:16:41 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2025/1261. Find Elements in a Contaminated Binary Tree/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    FindElements(TreeNode* root) {
+        
+    }
+    
+    bool find(int target) {
+        
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
+
+int main() {
+    Solution solution;
+    return 0;
+}
