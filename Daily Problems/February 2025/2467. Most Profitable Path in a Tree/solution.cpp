7a4fda910a963cc41c59f7ce70657b5df5f6fedd
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-02-24 02:20:34 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2025/2467. Most Profitable Path in a Tree/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
