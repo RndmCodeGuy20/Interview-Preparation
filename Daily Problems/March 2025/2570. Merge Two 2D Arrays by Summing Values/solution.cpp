9ca6f33d1ee1d67edd67cb7cada11f3f57417841
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-02 02:22:52 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2570. Merge Two 2D Arrays by Summing Values/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
