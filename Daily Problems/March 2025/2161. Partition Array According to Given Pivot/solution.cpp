6ed65e9b3d5af2b8f7767fd52c889d2ee6594998
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-03 02:21:10 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2161. Partition Array According to Given Pivot/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
