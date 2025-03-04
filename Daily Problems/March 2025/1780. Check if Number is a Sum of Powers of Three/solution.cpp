162725b80c54f50c8ca13d468385ba65181ffb39
Shantanu Mane <shantanu.mane.200@outlook.com>
2025-03-04 10:46:32 +0530

    feat: add solutions for LeetCode problems 1780 and 2570

--- Daily Problems/March 2025/1780. Check if Number is a Sum of Powers of Three/solution.cpp
+++ Daily Problems/March 2025/1780. Check if Number is a Sum of Powers of Three/solution.cpp
@@ -1,14 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
+private:
+    bool checkPowerHelperBrute(int power, int n)
+    {
+        if (n == 0)
+            return true;
+
+        if (n < pow(3, power))
+            return false;
+
+        bool addPower = checkPowerHelperBrute(power + 1, n - pow(3, power));
+        bool skipPower = checkPowerHelperBrute(power + 1, n);
+
+        return addPower || skipPower;
+    }
+
+    bool checkPowerHelperBetter(int n)
+    {
+        int power = 0;
+
+        while (pow(3, power) <= n)
+        {
+            power++;
+        }
+
+        while (n > 0)
+        {
+            if (n >= pow(3, power))
+                n -= pow(3, power);
+
+            if (n >= pow(3, power))
+                return false;
+
+            power--;
+        }
+
+        return true;
+    }
+
 public:
-    bool checkPowersOfThree(int n) {
-        
+    bool checkPowersOfThree(int n)
+    {
+        return checkPowerHelperBetter(n);
+        // return checkPowerHelperBrute(0, n);
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
+    vector<int> nums = {12, 91, 21};
+
+    for (auto &&n : nums)
+    {
+        cout << n << " solution :" << solution.checkPowersOfThree(n) << endl;
+    }
+
     return 0;
 }
--- Daily Problems/March 2025/2570. Merge Two 2D Arrays by Summing Values/solution.cpp
+++ Daily Problems/March 2025/2570. Merge Two 2D Arrays by Summing Values/solution.cpp
@@ -1,14 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        
+    vector<vector<int>> mergeArrays(vector<vector<int>> &nums1, vector<vector<int>> &nums2)
+    {
+        int ptr1 = 0, ptr2 = 0;
+        vector<vector<int>> rez;
+
+        while (ptr1 < nums1.size() && ptr2 < nums2.size())
+        {
+            if (nums1[ptr1][0] == nums2[ptr2][0])
+            {
+                rez.push_back({nums1[ptr1][0], nums1[ptr1][1] + nums2[ptr2][1]});
+                ptr1++;
+                ptr2++;
+            }
+            else if (nums1[ptr1][0] < nums2[ptr2][0])
+            {
+                rez.push_back({nums1[ptr1][0], nums1[ptr1][1]});
+                ptr1++;
+            }
+            else
+            {
+                rez.push_back({nums2[ptr2][0], nums2[ptr2][1]});
+                ptr2++;
+            }
+        }
+
+        if (ptr1 < nums1.size())
+        {
+            rez.insert(rez.end(), nums1.begin() + ptr1, nums1.end());
+        }
+        else
+        {
+            rez.insert(rez.end(), nums2.begin() + ptr2, nums2.end());
+        }
+
+        return rez;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
+    // vector<vector<int>> nums1 = {{1, 2},
+    //                              {2, 3},
+    //                              {4, 5}};
+    // vector<vector<int>>
+    //     nums2 = {{1, 4},
+    //              {3, 2},
+    //              {4, 1}};
+
+    vector<vector<int>> nums1 = {{2, 4},
+                                 {3, 6},
+                                 {5, 5}};
+    vector<vector<int>>
+        nums2 = {{1, 3},
+                 {8, 3}};
+
+    vector<vector<int>>
+        rez = solution.mergeArrays(nums1, nums2);
+
+    for (auto &&n : rez)
+    {
+        cout << n[0] << " " << n[1] << endl;
+    }
+
     return 0;
 }
