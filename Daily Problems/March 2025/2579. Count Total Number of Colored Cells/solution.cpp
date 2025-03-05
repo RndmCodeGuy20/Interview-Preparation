e7614d0ab681716df67517f3b75da60124a1e46f
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-05 02:20:24 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2579. Count Total Number of Colored Cells/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long coloredCells(int n) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
