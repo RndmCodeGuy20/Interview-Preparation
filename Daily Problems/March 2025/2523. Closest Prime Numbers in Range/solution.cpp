399167eece5bfd7fa46755b0843fec26c74928c5
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-07 02:21:18 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2523. Closest Prime Numbers in Range/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> closestPrimes(int left, int right) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
