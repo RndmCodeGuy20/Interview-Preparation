2ad87d6ff2b3e29a26afaa92b45435f29764fb47
Shantanu Mane <shantanu.mane.200@outlook.com>
2025-03-10 10:06:10 +0530

    solutions

--- Daily Problems/March 2025/2379. Minimum Recolors to Get K Consecutive Black Blocks/solution.cpp
+++ Daily Problems/March 2025/2379. Minimum Recolors to Get K Consecutive Black Blocks/solution.cpp
@@ -1,14 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
+private:
+    int countMinRecolors(string blocks)
+    {
+        int count = 0;
+        for (int i = 0; i < blocks.size(); i++)
+        {
+            if (blocks[i] == 'W')
+                count++;
+        }
+
+        return count;
+    }
+
 public:
-    int minimumRecolors(string blocks, int k) {
-        
+    int minimumRecolors(string blocks, int k)
+    {
+        int rez = INT_MAX;
+
+        if (blocks.size() == k)
+            return countMinRecolors(blocks);
+
+        for (int i = 0; i <= blocks.size() - k; i++)
+        {
+            string block = blocks.substr(i, k);
+            cout << "current block: " << block << endl;
+            rez = min(rez, countMinRecolors(block));
+        }
+
+        return rez;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
+
+    string blocks = "WBBWWBBWBW";
+    // string blocks = "WBWBBBW";
+    // string blocks = "WBBWWBB";
+    // string blocks = "WWBBBWBBBBBWWBWWWB";
+    // int k = 16;
+    int k = 7;
+    // int k = 2;
+
+    cout
+        << "minimum recolors: " << solution.minimumRecolors(blocks, k) << endl;
+
     return 0;
 }
--- Daily Problems/March 2025/2579. Count Total Number of Colored Cells/solution.cpp
+++ Daily Problems/March 2025/2579. Count Total Number of Colored Cells/solution.cpp
@@ -1,14 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    long long coloredCells(int n) {
-        
+    long long coloredCells(int n)
+    {
+        return ((long long)n * (n - 1) * 2) + 1;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
+
+    vector<int> n = {
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+    };
+
+    vector<long long> resz;
+
     return 0;
 }
