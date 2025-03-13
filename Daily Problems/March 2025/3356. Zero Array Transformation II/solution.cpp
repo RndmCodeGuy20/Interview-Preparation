9bf3d2dc8de83c12fc69095b4bdffe096c8ad7bf
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-13 02:22:33 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/3356. Zero Array Transformation II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
