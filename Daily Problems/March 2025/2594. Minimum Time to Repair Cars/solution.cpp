54b49e91bb6a28ceb179a125f67b0b566a0b2517
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-16 02:26:28 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2594. Minimum Time to Repair Cars/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
