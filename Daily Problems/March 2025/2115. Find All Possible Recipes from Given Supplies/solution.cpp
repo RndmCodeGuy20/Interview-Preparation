d4321e8996339aaf42e542d6d7c909bbb47c7f43
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-21 02:23:25 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2115. Find All Possible Recipes from Given Supplies/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
