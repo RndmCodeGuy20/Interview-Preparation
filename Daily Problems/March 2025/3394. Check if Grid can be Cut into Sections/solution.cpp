43cece737f0f0389e5f59eadca592243a2f52e7d
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-25 02:24:38 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/3394. Check if Grid can be Cut into Sections/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
