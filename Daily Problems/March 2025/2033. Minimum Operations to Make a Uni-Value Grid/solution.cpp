597a86a99ca0f5d923d360f5583d4fb33b0255a2
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-26 02:23:34 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2033. Minimum Operations to Make a Uni-Value Grid/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
