1a3c5211b428660a2573302afdeb5dc445d8c902
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-03-31 02:37:26 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2025/2551. Put Marbles in Bags/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
