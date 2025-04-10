bddbffe3c665413f101784480a593c5a073c0372
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-04-10 02:25:31 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2025/2999. Count the Number of Powerful Integers/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
