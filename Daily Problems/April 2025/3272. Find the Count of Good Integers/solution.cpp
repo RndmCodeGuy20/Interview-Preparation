4e9fe15f0e1dc60866cb6184d6a055203c78ee53
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-04-12 02:23:13 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2025/3272. Find the Count of Good Integers/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long countGoodIntegers(int n, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
