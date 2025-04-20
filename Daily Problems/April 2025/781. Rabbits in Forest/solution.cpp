0f0bbf1073a6e1654aacf642651f677363e4864c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-04-20 02:39:34 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2025/781. Rabbits in Forest/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
