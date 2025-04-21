2058390b5e65f947b10397b27b324f254acc620a
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-04-21 02:40:34 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2025/2145. Count the Hidden Sequences/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
