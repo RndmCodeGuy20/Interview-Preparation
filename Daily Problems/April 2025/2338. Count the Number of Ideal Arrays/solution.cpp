786d3171c56ab2beb1c1080d99c638f71765d034
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-04-22 02:27:41 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2025/2338. Count the Number of Ideal Arrays/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int idealArrays(int n, int maxValue) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
