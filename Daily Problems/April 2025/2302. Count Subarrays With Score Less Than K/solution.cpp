da591499118e6121a4e4843cec5a1c61fa377e84
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-04-28 02:41:15 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2025/2302. Count Subarrays With Score Less Than K/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
