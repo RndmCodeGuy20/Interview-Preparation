2aa8c1ff8662d8bc4bbe4688f791d1b21eda8c0b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-01 02:46:55 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/2071. Maximum Number of Tasks You Can Assign/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
