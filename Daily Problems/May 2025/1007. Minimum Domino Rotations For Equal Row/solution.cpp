2717881086e1e1097b93e20ea07e1a1b7a63af1d
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-03 02:27:25 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/1007. Minimum Domino Rotations For Equal Row/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
