1f84af8af94ad096fad01af20d0cd3219c460b0e
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-08 02:41:06 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/3342. Find Minimum Time to Reach Last Room II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
