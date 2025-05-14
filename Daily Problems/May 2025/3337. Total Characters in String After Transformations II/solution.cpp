b4ab961bd7aaa955653d32e84c11febf68e4de00
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-14 02:40:43 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/3337. Total Characters in String After Transformations II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t, vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
