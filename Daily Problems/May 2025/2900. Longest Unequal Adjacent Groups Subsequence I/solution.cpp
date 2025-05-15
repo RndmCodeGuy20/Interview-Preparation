0009dd31b4a0b7b492d77a7470c1fd82db5db04d
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-15 02:39:38 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/2900. Longest Unequal Adjacent Groups Subsequence I/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
