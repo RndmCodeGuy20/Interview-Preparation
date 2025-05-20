ca5f379e3be08239ec4fe0d57a03683151112445
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-20 02:42:33 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/3355. Zero Array Transformation I/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
