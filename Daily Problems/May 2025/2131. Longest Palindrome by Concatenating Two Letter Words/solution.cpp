4e57342ee15ef92b68844bc2a0d416fad4384af5
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-25 02:49:43 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/2131. Longest Palindrome by Concatenating Two Letter Words/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
