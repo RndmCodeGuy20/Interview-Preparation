b8057e9f2b049b3c96ebe0b85f71751b33f9fd07
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-26 02:46:31 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/1857. Largest Color Value in a Directed Graph/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
