9bb00a60a1f120e86b52fbebca1ffc293ee934b7
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-29 02:43:12 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/3373. Maximize the Number of Target Nodes After Connecting Trees II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
