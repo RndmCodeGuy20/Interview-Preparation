90eefcfb0f6e04d680bd73ff05b0d9bb6f12de8c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-30 02:41:21 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/2359. Find Closest Node to Given Two Nodes/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
