73951c3e0b364292f61375bb42c3e3b45438528a
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-05-31 02:38:53 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/May 2025/909. Snakes and Ladders/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
