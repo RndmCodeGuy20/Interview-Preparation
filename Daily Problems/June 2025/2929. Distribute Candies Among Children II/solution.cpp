89b7950805c307f00d3f2ba43c18d00461c6f5be
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-01 03:01:46 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/2929. Distribute Candies Among Children II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
