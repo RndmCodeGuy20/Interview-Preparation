c70e2ee4df767b773c0dbc1cb6d8a9fb4aad92c9
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-02 02:49:44 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/135. Candy/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
