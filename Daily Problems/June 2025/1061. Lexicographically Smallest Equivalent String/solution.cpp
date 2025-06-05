35f4f6cec019b8e8d908c597b10c2afdc7abf195
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-05 02:44:57 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/1061. Lexicographically Smallest Equivalent String/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
