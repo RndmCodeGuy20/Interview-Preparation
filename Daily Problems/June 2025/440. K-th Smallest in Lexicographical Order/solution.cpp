c3716351f37cd3e6f6b7c656689c17731a1bc3ac
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-09 02:51:30 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/440. K-th Smallest in Lexicographical Order/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
