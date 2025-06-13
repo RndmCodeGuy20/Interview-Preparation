cc4fbe66cf0a2c0a541321d6023f511bafb1e34b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-13 02:46:42 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/2616. Minimize the Maximum Difference of Pairs/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
