0fbda87101c488a6c0579fa84a70ddfe5a649d39
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-14 02:41:08 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/2566. Maximum Difference by Remapping a Digit/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
