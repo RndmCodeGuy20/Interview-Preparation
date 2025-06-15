3b6d38d3405daa2aeb48e78d511f102bab594359
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-15 02:54:03 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/1432. Max Difference You Can Get From Changing an Integer/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDiff(int num) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
