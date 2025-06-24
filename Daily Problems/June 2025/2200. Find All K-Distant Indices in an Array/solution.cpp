6c8df06c1bf4c9aca18ccbe1a3daaa9269fe0561
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-24 02:47:51 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/2200. Find All K-Distant Indices in an Array/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
