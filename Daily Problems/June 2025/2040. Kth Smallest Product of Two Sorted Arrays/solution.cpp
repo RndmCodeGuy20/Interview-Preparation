2417c939f9b18aa58b6458c9a1279d74ad5b307c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-25 02:48:47 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/2040. Kth Smallest Product of Two Sorted Arrays/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
