d3cbe754ce6fcfcc5d5812af1040d1a1022de223
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-06-29 02:57:57 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/June 2025/1498. Number of Subsequences That Satisfy the Given Sum Condition/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
