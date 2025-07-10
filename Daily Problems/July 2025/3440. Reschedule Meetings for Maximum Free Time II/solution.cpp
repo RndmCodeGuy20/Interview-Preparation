2b284d75e1d4297665e9a7a3b6c62a9ef240530e
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-07-10 02:51:07 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/July 2025/3440. Reschedule Meetings for Maximum Free Time II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
