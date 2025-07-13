6eefc1d502feeb687946138fbd3f6375b9dd1216
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-07-13 03:02:03 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/July 2025/2410. Maximum Matching of Players With Trainers/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
