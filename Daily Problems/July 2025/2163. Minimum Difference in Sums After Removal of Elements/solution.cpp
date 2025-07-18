8e4737992f4aed982bc9e9107f472b7919f17307
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-07-18 02:58:16 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/July 2025/2163. Minimum Difference in Sums After Removal of Elements/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumDifference(vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
