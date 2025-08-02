b53685da9ab53ebbbf0bc0d36e711f4836ddf1fd
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-02 02:53:03 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/2561. Rearranging Fruits/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
