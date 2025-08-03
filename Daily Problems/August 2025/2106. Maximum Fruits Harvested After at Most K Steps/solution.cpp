fa3f550f0f3fd29281f11406576407a9252bf588
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-03 03:09:53 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/2106. Maximum Fruits Harvested After at Most K Steps/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
