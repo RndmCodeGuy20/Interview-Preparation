8da42e4c9af101347ca37101e7facd67ff2e6ccb
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-05 03:04:46 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/3477. Fruits Into Baskets II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
