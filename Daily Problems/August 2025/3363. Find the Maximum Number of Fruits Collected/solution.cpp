8f0dd338f15fe1149855e06931b701991f149271
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-07 03:03:17 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/3363. Find the Maximum Number of Fruits Collected/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
