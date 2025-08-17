725e5fcc5be12b0a78896c838253373def4f580e
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-17 02:55:06 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/837. New 21 Game/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
