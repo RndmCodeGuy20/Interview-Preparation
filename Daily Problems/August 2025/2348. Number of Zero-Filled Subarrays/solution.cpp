f461a6d93b90984e1a5e266d4d0f2e41f72ca96c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-19 02:40:47 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/2348. Number of Zero-Filled Subarrays/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
