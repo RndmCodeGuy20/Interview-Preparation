beee35298c8e49254d8b1f91dc717bfe6a580e38
Shantanu Mane <shantanu.mane.200@outlook.com>
2025-08-22 09:32:52 +0530

    chore: daily problems completed

--- Daily Problems/August 2025/3195. Find the Minimum Area to Cover All Ones I/solution.cpp
+++ Daily Problems/August 2025/3195. Find the Minimum Area to Cover All Ones I/solution.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+
+class Solution
+{
 public:
-    int minimumArea(vector<vector<int>>& grid) {
-        
+    int minimumArea(vector<vector<int>> &grid)
+    {
+        int rez = 0;
+        pair<int, int> lC = {INT_MAX, INT_MAX};
+        pair<int, int> rC = {INT_MIN, INT_MIN};
+
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    // setting the left corner
+                    lC.first = min(lC.first, i);
+                    lC.second = min(lC.second, j);
+
+                    // setting the right corner
+                    rC.first = max(rC.first, i);
+                    rC.second = max(rC.second, j);
+                }
+            }
+        }
+
+        cout << "lC: (" << lC.first << ", " << lC.second << ")" << endl;
+        cout << "rC: (" << rC.first << ", " << rC.second << ")" << endl;
+
+        return (rC.first - lC.first + 1) * (rC.second - lC.second + 1);
     }
 };
 
