f447cc0807888ee4577c9d5204d8e5358055b8a4
Shantanu Mane <shantanu.mane.200@outlook.com>
2025-08-25 08:22:18 +0530

    chore: daily problems completed

--- Daily Problems/August 2025/1493. Longest Subarray of 1's After Deleting One Element/solution.cpp
+++ Daily Problems/August 2025/1493. Longest Subarray of 1's After Deleting One Element/solution.cpp
@@ -1,14 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestSubarray(vector<int>& nums) {
-        
+    int longestSubarray(vector<int> &nums)
+    {
+        int rez = 0;
+        int left = 0;
+        int count0 = 0;
+
+        for (int right = 0; right < nums.size(); right++)
+        {
+            if (nums[right] == 0)
+            {
+                count0++;
+            }
+
+            while (count0 > 1)
+            {
+                if (nums[left] == 0)
+                {
+                    count0--;
+                }
+                left++;
+            }
+
+            rez = max(rez, right - left + 1);
+        }
+
+        return rez;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
     return 0;
 }
--- Daily Problems/July 2025/2411. Smallest Subarrays With Maximum Bitwise OR/solution.cpp
+++ Daily Problems/July 2025/2411. Smallest Subarrays With Maximum Bitwise OR/solution.cpp
@@ -1,14 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
+private:
+    void updateSetters(int num, int index, vector<int> &setters)
+    {
+        int position = 0;
+        while (num > 0)
+        {
+            int bitValue = num % 2;
+            num /= 2;
+            if (bitValue == 1)
+            {
+                setters[position] = index;
+            }
+            position++;
+        }
+    }
+
 public:
-    vector<int> smallestSubarrays(vector<int>& nums) {
-        
+    vector<int> smallestSubarrays(vector<int> &nums)
+    {
+        vector<int> rez;
+        int maxOR;
+        vector<int> setters(32, -1);
+
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            maxOR |= nums[i];
+            updateSetters(nums[i], i, setters);
+            int nearestSetBit = *max_element(setters.begin(), setters.end());
+            rez.push_back(nearestSetBit - i + 1);
+        }
+
+        return rez;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
     return 0;
 }
