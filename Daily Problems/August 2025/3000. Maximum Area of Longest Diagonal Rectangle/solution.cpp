34be317cdc838623413b767daf3e762d78c09ad6
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-08-26 02:39:56 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/August 2025/3000. Maximum Area of Longest Diagonal Rectangle/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
