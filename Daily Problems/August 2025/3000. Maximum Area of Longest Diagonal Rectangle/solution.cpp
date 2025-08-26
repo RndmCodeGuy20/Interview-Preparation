de6d5e9ac664b445bc166213d41a21d837d08dcf
Shantanu Mane <shantanu.mane.200@outlook.com>
2025-08-26 09:42:08 +0530

    chore: daily problems completed

--- Daily Problems/August 2025/3000. Maximum Area of Longest Diagonal Rectangle/solution.cpp
+++ Daily Problems/August 2025/3000. Maximum Area of Longest Diagonal Rectangle/solution.cpp
@@ -1,14 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
+private:
+    float calculateDiagonal(int length, int width)
+    {
+        return sqrt(pow(length, 2) + pow(width, 2));
+    }
+
 public:
-    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        
+    int areaOfMaxDiagonal(vector<vector<int>> &dimensions)
+    {
+        int rez = 0;
+        float maxDiagonal = 0;
+
+        for (size_t i = 0; i < dimensions.size(); ++i)
+        {
+            vector<int> &dimension = dimensions[i];
+            float diagonal = calculateDiagonal(dimension[0], dimension[1]);
+            cout << "diagonal: " << diagonal << " for i:" << i << endl;
+            if (diagonal > maxDiagonal)
+            {
+                maxDiagonal = diagonal;
+                rez = dimension[0] * dimension[1];
+            }
+            else if (diagonal == maxDiagonal)
+            {
+                rez = max(rez, dimension[0] * dimension[1]);
+            }
+        }
+
+        return rez;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
+
+    // vector<vector<int>> dimensions = {{3, 4}, {5, 12}, {8, 15}, {7, 24}};
+    // cout << solution.areaOfMaxDiagonal(dimensions) << endl;
+
+    // vector<vector<int>> test = {{6, 8}, {10, 1}}; // (6,8) and (5,12) both have diagonal 10, but areas 48 and 60
+    // cout << solution.areaOfMaxDiagonal(test) << endl;
+
+    vector<vector<int>> dimensions = {{10, 3}, {5, 9}, {8, 3}};
+    cout << solution.areaOfMaxDiagonal(dimensions) << endl;
+
     return 0;
 }
--- Daily Problems/August 2025/498. Diagonal Traverse/solution.cpp
+++ Daily Problems/August 2025/498. Diagonal Traverse/solution.cpp
@@ -1,14 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        
+    vector<int> findDiagonalOrder(vector<vector<int>> &mat)
+    {
+        vector<int> rez;
+        map<int, vector<int>> indexMap;
+
+        for (int i = 0; i < mat.size(); i++)
+        {
+            for (int j = 0; j < mat[0].size(); j++)
+            {
+                int ind = i + j;
+                if (ind % 2 == 0)
+                    indexMap[ind].insert(indexMap[ind].begin(), mat[i][j]);
+                else
+                    indexMap[ind].push_back(mat[i][j]);
+            }
+        }
+
+        for (auto &pair : indexMap)
+        {
+            rez.insert(rez.end(), pair.second.begin(), pair.second.end());
+        }
+
+        return rez;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
+
+    vector<vector<int>> mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    // Since the current implementation does not return anything,
+    // we just call the function to ensure it runs without errors.
+    vector<int> rez = solution.findDiagonalOrder(mat);
+
+    for (int num : rez)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
