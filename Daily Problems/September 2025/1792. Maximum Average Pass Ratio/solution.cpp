e36498b452fc8025e05d508608a3b09c740c0f4b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-01 02:51:37 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/1792. Maximum Average Pass Ratio/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
