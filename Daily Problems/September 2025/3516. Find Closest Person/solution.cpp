4bbbf1f31b1d38e369ae1d4d1b1c838b39fac38e
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-04 02:18:53 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/3516. Find Closest Person/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
