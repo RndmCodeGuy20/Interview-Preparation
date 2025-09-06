8b7c9bc036659346634e68294ea6f9ec50b90baa
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-06 02:18:25 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/3495. Minimum Operations to Make Array Elements Zero/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minOperations(vector<vector<int>>& queries) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
