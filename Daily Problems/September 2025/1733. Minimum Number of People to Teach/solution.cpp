c441cc51e90822aacd8c5a382ec54f03d985c3e8
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-10 02:18:48 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/1733. Minimum Number of People to Teach/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
