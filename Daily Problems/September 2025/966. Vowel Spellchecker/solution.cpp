b3bdfedfe7ce9c56079b80ad66655314fe3d6881
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-14 02:25:48 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/966. Vowel Spellchecker/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
