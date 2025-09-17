3c6df5e795425fa16eca393121b737e786656f77
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-17 02:19:13 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/2353. Design a Food Rating System/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class FoodRatings {
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        
+    }
+    
+    void changeRating(string food, int newRating) {
+        
+    }
+    
+    string highestRated(string cuisine) {
+        
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
+
+int main() {
+    Solution solution;
+    return 0;
+}
