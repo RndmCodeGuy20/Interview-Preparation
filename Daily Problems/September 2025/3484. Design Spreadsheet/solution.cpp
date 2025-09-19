3c52f9897b87d3deed1b85fe30b4ee63b35c07fe
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-09-19 02:23:16 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/September 2025/3484. Design Spreadsheet/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Spreadsheet {
+public:
+    Spreadsheet(int rows) {
+        
+    }
+    
+    void setCell(string cell, int value) {
+        
+    }
+    
+    void resetCell(string cell) {
+        
+    }
+    
+    int getValue(string formula) {
+        
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
+
+int main() {
+    Solution solution;
+    return 0;
+}
