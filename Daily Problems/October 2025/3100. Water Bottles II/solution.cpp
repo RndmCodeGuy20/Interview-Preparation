6c6cc2a423505211eaa59721dd63f334223b1669
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-02 02:19:41 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/3100. Water Bottles II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
