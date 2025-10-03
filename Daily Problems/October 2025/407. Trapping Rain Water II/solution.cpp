377460c53a1dd1b21b85abcdac7c370ab9c6eeae
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-03 02:19:21 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/407. Trapping Rain Water II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
