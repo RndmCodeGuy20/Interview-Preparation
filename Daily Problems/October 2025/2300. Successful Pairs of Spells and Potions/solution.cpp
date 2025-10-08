2375f344b85994bcb78838e0c93f2787438ef127
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-08 02:20:09 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/2300. Successful Pairs of Spells and Potions/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
