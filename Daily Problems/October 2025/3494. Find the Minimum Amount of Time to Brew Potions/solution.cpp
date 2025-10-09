1a190de1f5b25be5cb5cfd6aff785d1ec4526a95
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-09 02:22:07 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/3494. Find the Minimum Amount of Time to Brew Potions/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
