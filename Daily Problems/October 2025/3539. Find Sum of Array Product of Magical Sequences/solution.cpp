1b0bc27ec89ad091b4b352f62efc59c21d50e418
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-12 02:24:57 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/3539. Find Sum of Array Product of Magical Sequences/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int magicalSum(int m, int k, vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
