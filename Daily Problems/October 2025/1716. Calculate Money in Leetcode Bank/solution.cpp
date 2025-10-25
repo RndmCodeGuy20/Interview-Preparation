03989e5b17263be023924af2d8c82596969e1cf5
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-25 02:23:45 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/1716. Calculate Money in Leetcode Bank/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int totalMoney(int n) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
