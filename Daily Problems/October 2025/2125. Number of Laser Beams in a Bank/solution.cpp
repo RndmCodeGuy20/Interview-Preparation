cefa35ad6b9fbee83d548e8ed61e8137ac2d2222
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-10-27 02:48:23 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/October 2025/2125. Number of Laser Beams in a Bank/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
