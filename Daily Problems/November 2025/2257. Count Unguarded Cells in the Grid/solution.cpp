a0450bcb2d98b49f58e9e5d4e5b65a82de8ea646
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-02 02:45:11 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/2257. Count Unguarded Cells in the Grid/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
