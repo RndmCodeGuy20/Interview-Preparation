93299ce2ad4b266119a4bd7e2da95144f1110463
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-03 02:46:13 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/1578. Minimum Time to Make Rope Colorful/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
