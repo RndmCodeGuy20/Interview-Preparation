706ebfd8efddbda0858a3fbbf355c5c7bc4af98b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-07 02:41:03 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/2528. Maximize the Minimum Powered City/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maxPower(vector<int>& stations, int r, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
