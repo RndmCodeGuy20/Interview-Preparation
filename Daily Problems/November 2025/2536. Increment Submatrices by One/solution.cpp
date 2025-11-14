baf518c5d4a6827ec63f174f56f15c5c07b0c79e
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-14 02:42:07 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/2536. Increment Submatrices by One/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
