fa75ade6aae7d4a85b5d2f39b02d0e3272f663de
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-17 02:44:38 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/1437. Check If All 1's Are at Least Length K Places Away/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
