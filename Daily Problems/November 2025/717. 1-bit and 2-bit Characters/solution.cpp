59f54ddbc7e2f43550def85e5a2032cf53e2b766
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-18 02:41:50 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/717. 1-bit and 2-bit Characters/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
