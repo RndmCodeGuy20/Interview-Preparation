cf1ba464d3724867c6623c0eab3e605f54579f98
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-28 02:41:06 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/2872. Maximum Number of K-Divisible Components/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
