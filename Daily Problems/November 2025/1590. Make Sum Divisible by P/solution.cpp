7aad6bd020824dcce47ba79cbb07773c907175f9
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-11-30 02:55:41 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/November 2025/1590. Make Sum Divisible by P/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
