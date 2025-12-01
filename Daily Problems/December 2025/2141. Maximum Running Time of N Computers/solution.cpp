b7ea5c0d30f609922f14506938ccabd9a7d90a2b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-01 03:07:05 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/2141. Maximum Running Time of N Computers/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
