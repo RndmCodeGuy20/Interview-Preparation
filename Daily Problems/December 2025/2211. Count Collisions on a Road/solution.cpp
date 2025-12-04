b82e9923af3be2c9566e6acd5b5047448b6c3a7c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-04 02:47:38 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/2211. Count Collisions on a Road/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countCollisions(string directions) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
