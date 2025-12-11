f1b4c1dd286996845e593527e026c30e221631cf
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-11 02:52:57 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/3531. Count Covered Buildings/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
