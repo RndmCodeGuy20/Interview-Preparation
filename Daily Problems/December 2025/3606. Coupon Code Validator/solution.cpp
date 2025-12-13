2429702fbfaf53e5be06ab2feb11ec0e63d21ebf
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-13 02:44:00 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/3606. Coupon Code Validator/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
