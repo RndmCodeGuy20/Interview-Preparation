4e3d5aea53bc5ecfcb7a5b557c8395891e1d820b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-16 02:52:37 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/3562. Maximum Profit from Trading Stocks with Discounts/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(int n, vector<int>& present, vector<int>& future, vector<vector<int>>& hierarchy, int budget) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
