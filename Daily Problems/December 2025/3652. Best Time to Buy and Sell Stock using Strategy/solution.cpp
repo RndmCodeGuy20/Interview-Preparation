673b939306b18b0efebd97bd526d3d3da8e9d437
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-18 02:49:11 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/3652. Best Time to Buy and Sell Stock using Strategy/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
