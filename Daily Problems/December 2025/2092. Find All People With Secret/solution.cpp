80ea166ce7c3ab0bccf985762f84c86dd7599264
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-19 02:52:07 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/2092. Find All People With Secret/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
