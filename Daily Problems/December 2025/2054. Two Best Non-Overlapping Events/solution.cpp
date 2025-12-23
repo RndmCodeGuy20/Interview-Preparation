e1d20b361a7e849606d22a5f3209499f328fcac4
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-23 02:54:23 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/2054. Two Best Non-Overlapping Events/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
