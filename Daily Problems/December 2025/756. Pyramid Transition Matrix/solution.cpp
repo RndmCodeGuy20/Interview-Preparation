c6b96ab70ffff6604784e3f3552ee8480af1edbf
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-29 03:05:30 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/756. Pyramid Transition Matrix/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool pyramidTransition(string bottom, vector<string>& allowed) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
