0fe3e27762b3ae8a203cfdd103d321c34fc45ec1
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2025-12-31 02:54:32 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2025/1970. Last Day Where You Can Still Cross/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
