152187d9cb2ac8eb88ff0a8226262fff637d5e93
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-01-03 02:49:25 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/January 2026/1411. Number of Ways to Paint N × 3 Grid/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numOfWays(int n) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
