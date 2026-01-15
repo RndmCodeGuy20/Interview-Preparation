b96d57f41f2e5869d6503b65973b85f25b4260d9
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-01-15 02:57:52 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/January 2026/2943. Maximize Area of Square Hole in Grid/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
