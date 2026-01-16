5c00206377649c3c8b285b2a9a8574b0ea6adb4c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-01-16 02:58:55 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/January 2026/2975. Maximum Square Area by Removing Fences From a Field/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
