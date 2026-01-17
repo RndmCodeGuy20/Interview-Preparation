f643751939bee732183a8b32c2dc80e4b460efdc
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-01-17 02:50:07 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/January 2026/3047. Find the Largest Area of Square Inside Two Rectangles/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
