63bf7f5ff68f0e2ee0477d4ee9d03fc5e18e1c97
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-01-29 03:26:10 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/January 2026/2976. Minimum Cost to Convert String I/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
