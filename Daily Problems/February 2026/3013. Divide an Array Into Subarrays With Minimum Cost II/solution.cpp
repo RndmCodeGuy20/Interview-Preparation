4001d8d16bdc42ff6548bdb6dba6799f4f8d8db8
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-02-02 03:40:39 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2026/3013. Divide an Array Into Subarrays With Minimum Cost II/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
