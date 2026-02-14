bb7bf07c1bd03c6e3912e1f344aef062a618ba4c
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-02-14 03:27:36 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2026/799. Champagne Tower/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
