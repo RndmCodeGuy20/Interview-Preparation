c35ee3e79a458143d253b05d561f7d7a38a0f027
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-02-18 03:37:27 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/February 2026/693. Binary Number with Alternating Bits/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
