6e94ecb6ff4bfee4b53ed2dff6e2ec484e6c0efe
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-03-04 03:25:18 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2026/1582. Special Positions in a Binary Matrix/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
