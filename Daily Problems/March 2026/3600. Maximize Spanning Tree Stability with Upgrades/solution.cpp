3426a310abcd8a627d23ee388d231dbc7c2caf21
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-03-12 03:31:51 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2026/3600. Maximize Spanning Tree Stability with Upgrades/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxStability(int n, vector<vector<int>>& edges, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
