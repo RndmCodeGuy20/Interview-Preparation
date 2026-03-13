ab67486ecfb1c69d106e524e007603c351303a3f
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-03-13 03:28:42 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2026/3296. Minimum Number of Seconds to Make Mountain Height Zero/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
