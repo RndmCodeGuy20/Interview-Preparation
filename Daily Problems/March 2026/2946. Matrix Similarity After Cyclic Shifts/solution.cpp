d07c3e99440137e2001b38122f096ec14cb42183
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-03-27 03:48:55 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2026/2946. Matrix Similarity After Cyclic Shifts/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
