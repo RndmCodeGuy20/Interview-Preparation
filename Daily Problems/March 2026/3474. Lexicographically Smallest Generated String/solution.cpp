8f235a1eff862d778ddc5d97f36c8d70e7ec0132
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-03-31 03:49:22 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/March 2026/3474. Lexicographically Smallest Generated String/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string generateString(string str1, string str2) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
