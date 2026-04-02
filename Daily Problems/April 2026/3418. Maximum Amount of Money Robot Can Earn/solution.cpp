45dcb75e217daddf49c7aadfcb1e2fba68111604
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-04-02 03:42:53 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2026/3418. Maximum Amount of Money Robot Can Earn/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
