39fd8ec4cc557eca64a7405f3ae2e84d39ea1fc0
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-04-03 03:44:44 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2026/3661. Maximum Walls Destroyed by Robots/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxWalls(vector<int>& robots, vector<int>& distance, vector<int>& walls) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
