ecb63834e8a7544326d61cea934cd08694ade103
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2026-04-14 04:09:07 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/April 2026/2463. Minimum Total Distance Traveled/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
